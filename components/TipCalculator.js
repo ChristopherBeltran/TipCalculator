09f46ba1968faff2bd40eeae42f1d23c0924d5be
ChristopherBeltran <christod.beltran@gmail.com>
2024-02-09 13:48:02 -0700

    Stubs out TipCalculator component

---
+++ components/TipCalculator.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+
+const TipCalculator = () => {
+  return <View>{}</View>;
+};
+
+export default TipCalculator;
